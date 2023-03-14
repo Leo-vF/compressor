d9d47f9eccfc9bf1eb3706b1e610973ccfa1f8ed
David Lau <davidlau@hotmail.de>
2023-03-14 23:01:43 +0100

    added testcase for 'create_huffman_tree_from_bytestream'

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use crate::huffman::{create_huffman_tree, create_huffman_tree_from_bytestream};
+
 mod files;
 mod huffman_tree;
 mod huffman;
 
 fn main() {
     println!("Hello, world!");
+    let mut test: Vec<u8> = Vec::new();
+    test.push(12);
+    test.push(12);
+    test.push(12);
+    test.push(12);
+    test.push(42);
+    test.push(42);
+    test.push(12);
+    test.push(42);
+    test.push(23);
+    test.push(21);
+
+    create_huffman_tree_from_bytestream(test);
 }
