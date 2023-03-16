4f9ef33bca662de3076acb3d5ba0ec5c1bb5fe29
David Lau <davidlau@hotmail.de>
2023-03-16 12:27:23 +0100

    Rewrote testcases.

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,10 @@ mod huffman_tree;
 mod huffman;
 
 fn main() {
-    println!("Hello, world!");
-    let mut test_0: Vec<u8> = Vec::new();
-    test_0.push(12);
-    test_0.push(12);
-    test_0.push(12);
-    test_0.push(12);
-    test_0.push(42);
-    test_0.push(42);
-    test_0.push(12);
-    test_0.push(42);
-    test_0.push(23);
-    test_0.push(21);
-
-    let tree = create_huffman_tree_from_bytestream(&test_0);
-    let test_1 = get_hashmap_for_compression(Box::from(tree));
-    println!("{}❤️", test_1.len());
-    let test_2 = get_huffman_code_header_for_file(test_1);
-    println!("{:?}", test_2);
-    let test_3 = get_hashmap_for_decompression(test_2);
-    println!("{}❤️", test_3.len())
+    let file: Vec<u8> = vec![12, 12, 12, 12, 42, 42, 12, 42, 23, 21];
+    println!("{:?}", file);
+    let compressed_file = compress(file);
+    println!("{:?}", compressed_file.data);
+    let decompressed_file = decompress(compressed_file);
+    println!("{:?}", decompressed_file);
 }
