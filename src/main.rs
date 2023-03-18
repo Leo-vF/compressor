32504546f39d0767634d6b0f4945d1cb3a8bb77b
David Lau <davidlau@hotmail.de>
2023-03-18 14:31:04 +0100

    added Time measurement for Optimization

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::time::Instant;
 
 mod files;
 mod hashing;
@@ -14,11 +15,17 @@ fn main() {
 
     if "d".eq(&args[1]) {
         let comp_file = files::read_comp_file(&comp_filepath);
+        let start = Instant::now();
         let decompressed_file = huffman::decompress(comp_file);
+        let duration = start.elapsed();
+        println!("Time needed for decompression: {:?}", duration);
         files::write_file(&normal_filepath, decompressed_file);
     } else if "c".eq(&args[1]) {
         let decompressed_file = files::read_file(&normal_filepath);
+        let start = Instant::now();
         let mut comp_file = huffman::compress(decompressed_file);
+        let duration = start.elapsed();
+        println!("Time needed for compression: {:?}", duration);
         files::write_comp_file(&comp_filepath, &mut comp_file);
     }
 }
